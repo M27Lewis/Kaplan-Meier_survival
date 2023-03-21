6b28cc51239598eeba60c88f7d15bf6d44d55be4
M27Lewis <michael27lewis@gmail.com>
2023-03-20 20:05:13 -0400

    Finished writing script to process raw MB cBP data to go into KM plot script. Saved output as RDS files.

--- scripts/processing/Process_MB_cBP_data_for_KM_script.R
+++ scripts/processing/Process_MB_cBP_data_for_KM_script.R
@@ -1,9 +1,26 @@
+library(tidyverse)
 
+# Set raw data paths
 MB_path <- "data/METABRIC_cBioPortal/data_mrna_agilent_microarray.txt"
 
-MB_z_path <- "data/METABRIC_cBioPortal/data_mrna_agilent_microarray_zscores_ref_all_samples.txt"
+MB_z_path <- "data/METABRIC_cBioPortal/data_mrna_agilent_microarray_zscores_ref_all_samples.txt" #Z-scored data from cBioPortal
 
+# Import data
 MB_data <- read.delim(MB_path, sep = "\t", header = T)
 
 MB_z_data <- read.delim(MB_z_path, sep = "\t", header = T)
 
+# Convert gene names column to rownames and remove Entrez IDs
+MB_data <- MB_data |> 
+  column_to_rownames(var = "Hugo_Symbol") |> 
+  select(!Entrez_Gene_Id)
+
+MB_z_data <- MB_z_data |> 
+  column_to_rownames(var = "Hugo_Symbol") |> 
+  select(!Entrez_Gene_Id)
+
+
+# Export processed data as RDS file
+saveRDS(MB_data, "data/METABRIC_cBioPortal/METABRIC_cBioPortal_raw_data.rds")
+
+saveRDS(MB_z_data, "data/METABRIC_cBioPortal/METABRIC_cBioPortal_z-scored_data.rds")
