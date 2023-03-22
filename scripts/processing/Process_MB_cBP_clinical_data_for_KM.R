ec8bd94dafc030b978a1c5e9323184c89532bb71
M27Lewis <michael27lewis@gmail.com>
2023-03-22 09:57:22 -0400

    Updates to the clinical processing script

--- scripts/processing/Process_MB_cBP_clinical_data_for_KM.R
+++ scripts/processing/Process_MB_cBP_clinical_data_for_KM.R
@@ -1,8 +1,25 @@
 library(tidyverse)
+library(janitor)
 
 # Set raw data paths
 MB_clin_path <- "data/METABRIC_cBioPortal/data_clinical_patient.txt"
 
 # Import data
 MB_clin_data <- read.delim(MB_clin_path, sep = "\t", header = F)
+
+# Prepare data for KM plot
+MB_clin_data <- MB_clin_data |> 
+  slice(5:n()) |> #Remove first 4 rows
+  janitor::row_to_names(row_number = 1) |> #Set first row as column names
+  select(PATIENT_ID, 
+         OS_MONTHS, 
+         OS_STATUS, 
+         CLAUDIN_SUBTYPE, 
+         VITAL_STATUS, 
+         RFS_STATUS, 
+         RFS_MONTHS) |> #Select columns needed for analysis
+  mutate_all(~na_if(., '')) |> #Recode empty values as NAs
+  na.omit()
+  
+
 
